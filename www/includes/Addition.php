efdb14f7ec895479b2c8a4de4c0a68b9751d1ba9
BenjaminThylin <benjamin.thylin@live.com>
2018-12-18 16:43:28 +0200

    calc changes

---
+++ www/includes/Addition.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<input id="a" type="text"> +
+<input id="b" type="text">
+    <script>
+    function addition(a ,b) {
+        return a + b;
+
+    }
+    </script>
+</body>
+</html>
--- www/single_mathsubject.php
+++ www/single_mathsubject.php
@@ -16,16 +16,8 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
 	// Ouput mathsubject info
 	while($row = mysqli_fetch_assoc($result)) {
 		echo '<h2>' . $row['title'] . '</h2>';
-		echo '<form oninput="x.value=parseInt(a.value)+parseInt(b.value)">
-				<input type="text" id="a" placeholder="Number">
-				+ <input type="text" id="b" placeholder="Number">
-				= <output type="text" name="x" for="a b"></output>
-				</form>';
-		// echo '<input class="" type="text" name="term1" placeholder="Number"> + ';
-		// //echo '<p> + </p>';
-		// echo '<input class="" type="text" name="term2" placeholder="Number">';
-		// echo'=<output name="x" for="a b"></output>';
-		// echo '<br>';
+		include 'includes/' . $row['title'] . '.php';
+		echo '<br>';
     	echo $row['theory'];
 	}
 	//echo '</ul>';
