936e04fb81e21765221043599dfe540d33d1761c
BenjaminThylin <benjamin.thylin@live.com>
2018-12-19 14:08:01 +0200

    Addition calc finished

--- www/includes/Addition.php
+++ www/includes/Addition.php
@@ -3,10 +3,17 @@
 
 <input id="a" type="text"> +
 <input id="b" type="text">
+<input id="answer" type="text">
     <script>
-    function addition(a ,b) {
-        return a + b;
+    document.getElementById("a").addEventListener("keyup", myFunction);
+    document.getElementById("b").addEventListener("keyup", myFunction);
 
+    function myFunction() {
+    var z = document.getElementById("answer");
+    var x = document.getElementById("a").value;
+    var y = document.getElementById("b").value;
+    z.value = Number(x) + Number(y);
+        
     }
     </script>
 </body>
